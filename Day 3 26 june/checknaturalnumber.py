548cb7e2ff2d792fe9407c1044b4fa080346a845
dixitanshika458 <dixitanshika458@gmail.com>
2026-06-26 10:58:43 +0530

    check natural number

---
+++ Day 3 26 june/checknaturalnumber.py
@@ -0,0 +1,16 @@
+#to check if a number is a natural number
+print("--------------------------------------------")
+
+number = int(input("Enter a number: "))
+#to validate the number
+if(number > 0):
+    print("The number is a natural number.")
+    #---------------------------------------
+else:
+    print("The number is not a natural number.")
+print("--------------------------------------------")
+#-----------------------------------------------------
+#output:
+#Enter a number: 5
+#The number is a natural number.
+#-----------------------------------------------------
