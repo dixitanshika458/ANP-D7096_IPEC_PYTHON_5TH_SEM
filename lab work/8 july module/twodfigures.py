1ae0acdcd26358ffcea3829dce8d0ab7fd5c77d0
dixitanshika458 <dixitanshika458@gmail.com>
2026-07-08 11:57:02 +0530

    calculating two d figures

---
+++ lab work/8 july module/twodfigures.py
@@ -0,0 +1,40 @@
+#making triangle area function
+def triangle_area(base, height):
+    area = 0.5 * base * height
+    return area
+#------------------------------------------------
+#making triangle perimeter function
+def triangle_perimeter(side1, side2, side3):
+    perimeter = side1 + side2 + side3
+    return perimeter
+#------------------------------------------------
+#making circle area function
+def circle_area(radius):
+    area = 3.14159 * radius ** 2
+    return area
+#------------------------------------------------
+#making circle circumference function
+def circle_circumference(radius):
+    circumference = 2 * 3.14159 * radius
+    return circumference
+#------------------------------------------------
+#making square area function
+def square_area(side):
+    area = side ** 2
+    return area
+#------------------------------------------------
+#making square perimeter function
+def square_perimeter(side):
+    perimeter = 4 * side
+    return perimeter
+#------------------------------------------------
+#making rectangle area function
+def rectangle_area(length, width):
+    area = length * width
+    return area
+#------------------------------------------------
+#making rectangle perimeter function
+def rectangle_perimeter(length, width):
+    perimeter = 2 * (length + width)
+    return perimeter
+#-------------------------------------------------
