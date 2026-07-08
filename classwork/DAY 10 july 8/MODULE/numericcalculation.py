3c8d8c806d09021f459421a615530f3f142581f9
dixitanshika458 <dixitanshika458@gmail.com>
2026-07-08 10:41:04 +0530

    module

---
+++ classwork/DAY 10 july 8/MODULE/numericcalculation.py
@@ -0,0 +1,16 @@
+#creating module for calculation
+#creating function for addition
+def calculate_addition(num1, num2):
+    return num1 + num2
+#creating function for subtraction
+def calculate_subtraction(num1, num2):
+    return num1 - num2
+#creating function for multiplication
+def calculate_multiplication(num1, num2):
+    return num1 * num2
+#creating function for division
+def calculate_division(num1, num2):
+    return num1 / num2
+#creating function for remainder
+def calculate_remainder(num1, num2):
+    return num1 % num2
